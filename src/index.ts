53935d5db6330e7290c6fb4b0f81b24bb7a8a338
araguma <itetbitb@gmail.com>
2023-06-24 23:29:01 -0400

    Implement cp subcommand

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { version } from '../package.json';
 import fs from 'node:fs/promises';
+import { join } from 'node:path';
 import readline from 'node:readline/promises';
 import { Command }  from 'commander';
 import { glob } from 'glob';
@@ -27,13 +28,8 @@ async function rm(pattern: string, options: {
     const paths = await glob(pattern, {
         ignore: options.ignore,
     });
-    if(!options.force) {
-        const rl = readline.createInterface(process.stdin, process.stdout);
-        let response = (await rl.question(`Delete ${pattern}? (y) `)).toLowerCase();
-        rl.close();
-        if(response !== 'y' && response !== '')
-            return console.log('Operation canceled');
-    }
+    if(!options.force && !(await ask(`Delete ${pattern}? (y) `)).match(/y|^$/ig))
+        return console.log('Operation canceled');
     let removeCount = paths.length;
     await Promise.all(paths.map(async (path) => {
         await fs.rm(path, {
@@ -50,14 +46,34 @@ program.command('cp')
     .argument('<string>', 'Source files or directories')
     .argument('<string>', 'Destination directory')
     .option('-i, --ignore <string>', 'Files or directories to ignore')
-    .option('-w, --watch [string]', 'Starts watch mode', './')
+    .option('-w, --watch [string]', 'Starts watch mode on source or provided glob')
     .action(cp);
 
 async function cp(pattern: string, destPath: string, options: {
     ignore?: string,
-    watch?: boolean,
+    watch?: true | string,
 }) {
-
+    const srcPaths = await glob(pattern, {
+        ignore: options.ignore,
+    });
+    const copyToDest = (path: string) => {
+        fs.cp(path, join(destPath, path), {
+            recursive: true,
+        }).catch(() => {
+            
+        });
+    };
+    srcPaths.forEach(copyToDest);
+    if(options.watch === undefined) return;
+    chokidar.watch(options.watch === true ? srcPaths : options.watch)
+        .on('change', copyToDest);
 }
 
 program.parse();
+
+async function ask(prompt: string): Promise<string> {
+    const rl = readline.createInterface(process.stdin, process.stdout);
+    let response = await rl.question(prompt);
+    rl.close();
+    return response;
+}
